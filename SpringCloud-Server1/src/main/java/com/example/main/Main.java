877a287530cf994e9e49c9ff98bff3ed1f68b595
MilesChien <q543165@gmail.com>
2019-03-27 18:12:40 +0800

    單純針對其中一個subProject修改

--- SpringCloud-Server1/src/main/java/com/example/main/Main.java
+++ SpringCloud-Server1/src/main/java/com/example/main/Main.java
@@ -3,6 +3,7 @@ package com.example.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@EnableConfigServer
 @SpringBootApplication
 public class Main {
 
