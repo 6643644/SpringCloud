e1969dfbd31fc9ead6cba84ad39ff7ac1f09c0c7
MilesChien <q543165@gmail.com>
2019-04-02 14:27:40 +0800

    把其他webService project 一併統一成spring cloud project 專案

--- SpringCloud-Server1/src/main/java/com/example/main/Main.java
+++ SpringCloud-Server1/src/main/java/com/example/main/Main.java
@@ -14,7 +14,6 @@ import org.springframework.context.annotation.ComponentScan;
  * 注意:如果SpringBootAppliction的路口放在某個目錄底下，當我們啟動SpringBoot並且掃描Bean的產生時，只會掃描路口所屬的路徑底下。
  * 
  */
-
 @ComponentScan(basePackages = { "com.example" })
 @SpringBootApplication
 public class Main {
